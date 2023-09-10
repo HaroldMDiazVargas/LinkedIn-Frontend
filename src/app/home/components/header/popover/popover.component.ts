6049ae3747095fe44418bb175097265645cf0adf
Harold Diaz <harold.diazv1@gmail.com>
2023-09-10 13:45:58 -0500

    Add popover component

--- src/app/home/components/header/popover/popover.component.ts
+++ src/app/home/components/header/popover/popover.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-popover',
   templateUrl: './popover.component.html',
   styleUrls: ['./popover.component.scss'],
-  standalone: true
+  standalone: true,
+  imports: [IonicModule]
 })
 export class PopoverComponent  implements OnInit {
 
