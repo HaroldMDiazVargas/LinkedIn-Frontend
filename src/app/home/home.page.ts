039048dd8df6b1d1b16b701d293034668c13700b
Harold Diaz <harold.diazv1@gmail.com>
2023-09-10 21:17:59 -0500

    Add new profile, post and advertise component by ionic cli

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { HeaderComponent } from './components/header/header.component';
+import { StartPostComponent } from './components/start-post/start-post.component';
+import { AdversitingComponent } from './components/adversiting/adversiting.component';
+import { ProfileSummaryComponent } from './components/profile-summary/profile-summary.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule, HeaderComponent],
+  imports: [IonicModule, HeaderComponent, ProfileSummaryComponent, StartPostComponent, AdversitingComponent],
 })
 export class HomePage {
   constructor() {}
