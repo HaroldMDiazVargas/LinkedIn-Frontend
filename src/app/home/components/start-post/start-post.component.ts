bebc25afb8fe8425feb6c049823d2c2b079c9454
Harold Diaz <harold.diazv1@gmail.com>
2023-09-12 18:54:37 -0500

    Add font-awesome icons, post card to see user-account posts, set the layout

--- src/app/home/components/start-post/start-post.component.ts
+++ src/app/home/components/start-post/start-post.component.ts
@@ -1,14 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { IonicModule } from '@ionic/angular';
+import { faComment, faThumbsUp } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-start-post',
   templateUrl: './start-post.component.html',
   styleUrls: ['./start-post.component.scss'],
   standalone: true,
-  imports: [IonicModule]
+  imports: [IonicModule, FontAwesomeModule]
 })
 export class StartPostComponent  implements OnInit {
+  faThumbsUp = faThumbsUp;
+  faComment = faComment
 
   constructor() { }
 
