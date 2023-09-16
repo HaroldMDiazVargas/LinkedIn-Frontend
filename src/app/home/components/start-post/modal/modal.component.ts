0fbb43d056526705dc10e7e79bcc94ac2900c6eb
Harold Diaz <harold.diazv1@gmail.com>
2023-09-16 18:55:46 -0500

    Fix btn elements, form and disable btn to post

--- src/app/home/components/start-post/modal/modal.component.ts
+++ src/app/home/components/start-post/modal/modal.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 
 @Component({
@@ -6,7 +7,7 @@ import { IonicModule } from '@ionic/angular';
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.scss'],
   standalone: true,
-  imports: [IonicModule]
+  imports: [IonicModule, FormsModule]
 })
 export class ModalComponent  implements OnInit {
 
