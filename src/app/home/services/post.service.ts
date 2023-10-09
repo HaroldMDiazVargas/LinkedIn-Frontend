d594389bdd1074707fc5e60e9f10be347627791d
Harold Diaz <harold.diazv1@gmail.com>
2023-10-09 17:29:44 -0500

    Add http methods for CRUD post

--- src/app/home/services/post.service.ts
+++ src/app/home/services/post.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Post } from '../models/Post';
 import { environment } from 'src/environments/environment';
+import { take } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -14,5 +15,27 @@ export class PostService {
   getSelectedPosts(params: string){
     return this.http.get<Post[]>(this.baseUrl + params, { withCredentials: true})
   }
+
+  createPost(body: string){
+    return this.http.post<Post>(this.baseUrl, { body }, { withCredentials: true} )
+      .pipe(
+        take(1)       //Avoid to unsubcribe, destorys the observable after take one
+      );
+  }
+
+  updatePost(id: number, body: string){
+    return this.http.put(`${this.baseUrl}/${id}`, { body }, { withCredentials: true} )
+      .pipe(
+        take(1)       //Avoid to unsubcribe, destorys the observable after take one
+      );
+  }
+
+  deletePost(id: number){
+    return this.http.delete(`${this.baseUrl}/${id}`, { withCredentials: true} )
+      .pipe(
+        take(1)       //Avoid to unsubcribe, destorys the observable after take one
+      );
+  }
+
 
 }
