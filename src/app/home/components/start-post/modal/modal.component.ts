6097a48d3d4dea9ecfc710956481ad50f6a204ac
Harold Diaz <harold.diazv1@gmail.com>
2023-10-14 12:09:03 -0500

    Apply two-way binding on input field to pre-pop old body on ngInit

--- src/app/home/components/start-post/modal/modal.component.ts
+++ src/app/home/components/start-post/modal/modal.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { ModalController } from '@ionic/angular';
+import { Post } from 'src/app/home/models/Post';
 
 @Component({
   selector: 'app-modal',
@@ -11,12 +12,15 @@ import { ModalController } from '@ionic/angular';
   imports: [IonicModule, FormsModule]
 })
 export class ModalComponent  implements OnInit {
-  
+  body: string = '';
+  @Input() postToUpdate!: Post;
   @ViewChild('f') form!:NgForm;
 
   constructor(public modalController: ModalController) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.body = this.postToUpdate ? this.postToUpdate.body : '';
+  }
 
   onPost() {
     if (!this.form.valid) return;
