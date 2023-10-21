c4d9bacf6ff1c6909d23bc6fb3c5ffdadd0709ed
Harold Diaz <harold.diazv1@gmail.com>
2023-10-20 21:20:17 -0500

    Set name and image user subscription

--- src/app/home/components/start-post/modal/modal.component.ts
+++ src/app/home/components/start-post/modal/modal.component.ts
@@ -1,7 +1,9 @@
-import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { Component, Input, OnInit, ViewChild, OnDestroy } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { ModalController } from '@ionic/angular';
+import { Subscription } from 'rxjs';
+import { AuthService } from 'src/app/auth/services/auth.service';
 import { Post } from 'src/app/home/models/Post';
 
 @Component({
@@ -11,15 +13,27 @@ import { Post } from 'src/app/home/models/Post';
   standalone: true,
   imports: [IonicModule, FormsModule]
 })
-export class ModalComponent  implements OnInit {
+export class ModalComponent  implements OnInit, OnDestroy {
+  private subscriptions: Subscription[] = [];
+  userFullImagePath: string = '';
+  userFullName: string = '';
   body: string = '';
   @Input() postToUpdate!: Post;
   @ViewChild('f') form!:NgForm;
 
-  constructor(public modalController: ModalController) { }
+  constructor(
+    public modalController: ModalController,
+    private authService: AuthService
+    ) { }
 
   ngOnInit() {
     this.body = this.postToUpdate ? this.postToUpdate.body : '';
+    this.subscriptions.push(this.authService.userFullName.subscribe((userFullName: string) => this.userFullName = userFullName));
+    this.subscriptions.push(this.authService.userFullImagePath.subscribe((fullImagePath: string) => this.userFullImagePath = fullImagePath )); 
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach(sub => sub.unsubscribe());
   }
 
   onPost() {
