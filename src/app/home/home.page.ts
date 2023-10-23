ca5c247dba45fd09f1ae46008e5aa968e56fde32
Harold Diaz <harold.diazv1@gmail.com>
2023-10-22 22:02:01 -0500

    Set home page a route-outlet to dynamic inject child components

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,23 +1,14 @@
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { HeaderComponent } from './components/header/header.component';
-import { StartPostComponent } from './components/start-post/start-post.component';
-import { ProfileSummaryComponent } from './components/profile-summary/profile-summary.component';
-import { AdvertisingComponent } from './components/advertising/advertising.component';
-import { AllPostsComponent } from './components/all-posts/all-posts.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule, HeaderComponent, ProfileSummaryComponent, StartPostComponent, AdvertisingComponent, AllPostsComponent],
+  imports: [IonicModule, HeaderComponent],
 })
 export class HomePage {
-  body = '';
   constructor() {}
-
-  onCreatePost(body: string){
-    this.body = body;
-  }
 }
