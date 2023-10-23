3e90c1f249be1ac9422a5a22fc6f03337a061f71
Harold Diaz <harold.diazv1@gmail.com>
2023-10-23 17:30:42 -0500

    Consume endpoint to get status of the friend request

--- src/app/home/services/connection-profile.service.ts
+++ src/app/home/services/connection-profile.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { User } from 'src/app/auth/models';
 import { environment } from 'src/environments/environment';
+import { FriendRequestStatus } from '../models/FriendRequest';
 
 @Injectable({
   providedIn: 'root'
@@ -15,6 +16,10 @@ export class ConnectionProfileService {
   ) { }
 
   getConnectionUser(id: number): Observable<User>{
-    return this.http.get<User>(`${this.baseUrl}${id}`, { withCredentials: true})
+    return this.http.get<User>(`${this.baseUrl}${id}`, { withCredentials: true});
+  }
+
+  getFriendRequestStatus(id: number): Observable<FriendRequestStatus> {
+    return this.http.get<FriendRequestStatus>(`${this.baseUrl}friend-request/status/${id}`, { withCredentials: true });
   }
 }
