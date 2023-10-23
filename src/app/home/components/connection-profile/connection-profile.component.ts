27158b98bd4a83b78304b204d73f2cfd34576fb4
Harold Diaz <harold.diazv1@gmail.com>
2023-10-23 12:30:49 -0500

    Set markup and style, add responsive support

--- src/app/home/components/connection-profile/connection-profile.component.ts
+++ src/app/home/components/connection-profile/connection-profile.component.ts
@@ -1,17 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
+import { BannerColorService } from '../../services/banner-color.service';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-connection-profile',
   templateUrl: './connection-profile.component.html',
   styleUrls: ['./connection-profile.component.scss'],
   standalone: true,
-  imports: [HeaderComponent]
+  imports: [HeaderComponent, CommonModule, IonicModule]
 })
 export class ConnectionProfileComponent  implements OnInit {
+  userFullImagePath = '';
+  userFullName = '';
 
-  constructor() { }
+  constructor(
+    public bannerService: BannerColorService
+  ) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.userFullImagePath = environment.apiUrl+'/feed/image/avatar.svg';
+    this.userFullName = 'Harold Díaz'
+  }
 
 }
