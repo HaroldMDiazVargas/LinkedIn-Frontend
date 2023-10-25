8834ba3d28c1cda8a5236aedfdb251232feac436
Harold Diaz <harold.diazv1@gmail.com>
2023-10-25 17:34:41 -0500

    Set event binding on click and render according status dynamically

--- src/app/home/components/connection-profile/connection-profile.component.ts
+++ src/app/home/components/connection-profile/connection-profile.component.ts
@@ -8,7 +8,7 @@ import { ConnectionProfileService } from '../../services/connection-profile.serv
 import { ActivatedRoute, RouterModule } from '@angular/router';
 import { User } from 'src/app/auth/models';
 import { Observable, map, of, switchMap, take } from 'rxjs';
-import { FriendRequestStatus, FriendRequest_Status } from '../../models/FriendRequest';
+import { FriendRequest, FriendRequestStatus, FriendRequest_Status } from '../../models/FriendRequest';
 
 @Component({
   selector: 'app-connection-profile',
@@ -21,7 +21,7 @@ export class ConnectionProfileComponent  implements OnInit {
   user!: User;
   baseUserImageUrl: string = environment.apiUrl+'/feed/image/';
   bannerColors: BannerColors = {} as BannerColors;
-  friendRequestStatus: FriendRequest_Status = {} as FriendRequest_Status;
+  friendRequestStatus: FriendRequest = {} as FriendRequest;
 
   constructor(
     public bannerService: BannerColorService,
@@ -35,9 +35,9 @@ export class ConnectionProfileComponent  implements OnInit {
       this.user = user;
     });
 
-    this.getFriendRequestStatus().pipe(take(1)).subscribe((friendRequestStatus: FriendRequestStatus) => {
+    this.getFriendRequestStatus().pipe(take(1)).subscribe((friendRequestStatus: FriendRequest) => {
       console.log(friendRequestStatus)
-      this.friendRequestStatus = friendRequestStatus.status;
+      this.friendRequestStatus = friendRequestStatus;
     })
   }
 
@@ -50,13 +50,25 @@ export class ConnectionProfileComponent  implements OnInit {
     )
   }
   
-  getFriendRequestStatus(): Observable<FriendRequestStatus>{
+  getFriendRequestStatus(): Observable<FriendRequest>{
     return this.getUserIdFromUrl().pipe(
       take(1),
       switchMap((userId: number) => {
         return this.connectionService.getFriendRequestStatus(userId)
       })
       )
+  }
+
+  addConnection(status: FriendRequest_Status): void {
+    if (this.friendRequestStatus.status === 'waiting-for-current-user-response'){
+      const id = this.friendRequestStatus.id;
+      this.connectionService.respondFriendRequest(id, status).pipe(take(1)).subscribe();
+      this.friendRequestStatus.status = status;
+    }
+    else {
+      this.connectionService.sendFriendRequest(this.user.id).pipe(take(1)).subscribe();
+      this.friendRequestStatus.status = 'pending';
+    }
   }
 
   private getUserIdFromUrl(): Observable<number>{
