a9398529c7d258a25831e83f06646ebf55d7c742
Harold Diaz <harold.diazv1@gmail.com>
2023-10-25 20:37:54 -0500

    New component popover to display list of pending friend request

---
+++ src/app/home/components/header/friend-request-popover/friend-request-popover.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+
+@Component({
+  selector: 'app-friend-request-popover',
+  templateUrl: './friend-request-popover.component.html',
+  styleUrls: ['./friend-request-popover.component.scss'],
+  standalone: true,
+  imports: [IonicModule]
+})
+export class FriendRequestPopoverComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
