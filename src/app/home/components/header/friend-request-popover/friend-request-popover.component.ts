ebd494d03d4061449451e15ed98b722591a3858b
Harold Diaz <harold.diazv1@gmail.com>
2023-10-26 20:51:48 -0500

    Bind accept and decline btns and update reqeust data

--- src/app/home/components/header/friend-request-popover/friend-request-popover.component.ts
+++ src/app/home/components/header/friend-request-popover/friend-request-popover.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { IonicModule } from '@ionic/angular';
+import { IonicModule, PopoverController } from '@ionic/angular';
+import { take } from 'rxjs';
 import { ConnectionProfileService } from 'src/app/home/services/connection-profile.service';
 import { environment } from 'src/environments/environment';
 
@@ -15,9 +16,24 @@ export class FriendRequestPopoverComponent  implements OnInit {
   baseUserImageUrl: string = environment.apiUrl+'/feed/image/';
   
   constructor(
-    public connectionProfileService: ConnectionProfileService
+    public connectionService: ConnectionProfileService,
+    private popoverController: PopoverController
   ) { }
 
   ngOnInit() {}
+
+  acceptRequest(id: number){
+    this.connectionService.respondFriendRequest(id, 'accepted').pipe(take(1)).subscribe();
+    this.connectionService.friendRequests = this.connectionService.friendRequests.filter((request) => request.id !== id);
+    if (this.connectionService.friendRequests.length === 0)
+      this.popoverController.dismiss();
+  }
+
+  declineRequest(id: number){
+    this.connectionService.respondFriendRequest(id, 'declined').pipe(take(1)).subscribe();
+    this.connectionService.friendRequests = this.connectionService.friendRequests.filter((request) => request.id !== id);
+    if (this.connectionService.friendRequests.length === 0)
+      this.popoverController.dismiss();
+  }
 
 }
