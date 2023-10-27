c33d5935040e5f1842e6cab6c9ab011185437374
Harold Diaz <harold.diazv1@gmail.com>
2023-10-26 20:50:26 -0500

    Use controller popover instead inline to fire on click event

--- src/app/home/components/header/header.component.ts
+++ src/app/home/components/header/header.component.ts
@@ -1,36 +1,39 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { PopoverComponent } from './popover/popover.component';
-import { IonicModule } from '@ionic/angular';
+import { IonicModule, PopoverController } from '@ionic/angular';
 import { AuthService } from 'src/app/auth/services/auth.service';
-import { Subscription, take } from 'rxjs';
+import { BehaviorSubject, Subscription, take } from 'rxjs';
 import { ConnectionProfileService } from '../../services/connection-profile.service';
 import { FriendRequest } from '../../models/FriendRequest';
 import { FriendRequestPopoverComponent } from './friend-request-popover/friend-request-popover.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
   standalone: true,
-  imports: [PopoverComponent, FriendRequestPopoverComponent, IonicModule]
+  imports: [PopoverComponent, FriendRequestPopoverComponent, IonicModule, CommonModule]
 })
 export class HeaderComponent  implements OnInit, OnDestroy {
-  friendRequests: FriendRequest[] = [];
+  // countFriendRequests = new BehaviorSubject<number>(0);
   friendRequestsSubscription!: Subscription;
   private userImagePathsubscription!: Subscription;
   userFullImagePath: string = '';
+  // popoverController: any;
 
   
   constructor(
     private authService: AuthService,
     public connectionService: ConnectionProfileService,
+    private popoverController: PopoverController
   ) { }
 
   ngOnInit() {
     this.userImagePathsubscription = this.authService.userFullImagePath.subscribe((fullImagePath: string) => this.userFullImagePath = fullImagePath );
 
     this.friendRequestsSubscription = this.connectionService.getMyFriendRequestReceived().subscribe((friendRequests: FriendRequest[]) => {
-      this.friendRequests = friendRequests;
+      // this.countFriendRequests.next(friendRequests.length);
       this.connectionService.friendRequests = friendRequests;                                   //Global property on centralize service/ ALSO: we could do using Input props and pass to other component!
     });
   }
@@ -38,5 +41,22 @@ export class HeaderComponent  implements OnInit, OnDestroy {
   ngOnDestroy(): void {
     this.userImagePathsubscription.unsubscribe();
     this.friendRequestsSubscription.unsubscribe();
+  }
+
+  
+  async presentPopover(e: Event) {
+    if (this.connectionService.friendRequests.length === 0) return;
+    const popover = await this.popoverController.create({
+      component: FriendRequestPopoverComponent,
+      event: e,
+      showBackdrop: false,
+      // dismissOnSelect: true,
+      alignment: 'center',
+      side: 'bottom',
+      cssClass: 'request-popover'
+      
+    });
+
+    await popover.present();
   }
 }
