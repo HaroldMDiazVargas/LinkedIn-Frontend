9b193603aa7ed00363002b0be9d41bd2b7af066c
Harold Diaz <harold.diazv1@gmail.com>
2024-01-31 21:19:57 -0500

    New component spinner loader to share on all components

---
+++ src/app/shared/components/loading-spinner/loading-spinner.component.ts
@@ -0,0 +1,18 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+
+@Component({
+  selector: 'app-loading-spinner',
+  templateUrl: './loading-spinner.component.html',
+  styleUrls: ['./loading-spinner.component.scss'],
+  standalone: true,
+  imports: [IonicModule, CommonModule,]
+})
+export class LoadingSpinnerComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
